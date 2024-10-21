ef19d4cfd4317aba947e3bdf3efb9d64b9c494a9
Gleb Zikunov <zikunovga@gmail.com>
2024-10-21 14:38:01 +0300

    feat: modified project structure for cucumber

---
+++ configs/chrome.cucumber.conf.js
@@ -0,0 +1,22 @@
+import { downloadDir, mainConfig } from "../framework/configs/main.wdio.conf.js"
+
+export const config = {
+  ...mainConfig,
+  ...{
+    framework: "cucumber",
+    cucumberOpts: {
+      require: ["./test/step-definitions/**/*.js"],
+    },
+    specs: ["../test/features/**/*.feature"],
+    capabilities: [
+      {
+        browserName: "chrome",
+        "goog:chromeOptions": {
+          prefs: {
+            "download.default_directory": downloadDir,
+          },
+        },
+      },
+    ],
+  },
+}
