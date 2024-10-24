f7d85c41fcdec2d9b5936fa1312de129208b0142
Gleb Zikunov <zikunovga@gmail.com>
2024-10-25 00:43:28 +0300

    fix: add or remove await where it is needed

--- test/page-objects/communityMarketPage.js
+++ test/page-objects/communityMarketPage.js
@@ -1,96 +1,99 @@
-import { Button, Dropdown, Label } from "../../framework/elements/index.js"
-import BasePage from "../../framework/page/BasePage.js"
+import Browser from "../../framework/browser/Browser.js";
+import { Button, Dropdown, Label } from "../../framework/elements/index.js";
+import BasePage from "../../framework/page/BasePage.js";
 
 class CommunityMarketPage extends BasePage {
   constructor() {
-    super(new Label("//h1[@class='market_header']", "Community Market Header"), "Community Market Page")
+    super(new Label("//h1[@class='market_header']", "Community Market Header"), "Community Market Page");
 
-    this.advancedOptionsButton = new Button("//div[contains(@class, 'advanced_button')]", "Advanced Options Button")
-    this.advancedSearchWindow = new Label("//div[@id='market_advancedsearch_dialog']", "Advanced Search Window")
-    this.searchButton = new Button("//div[contains(@onclick, 'submit')]", "Search Button")
+    this.advancedOptionsButton = new Button("//div[contains(@class, 'advanced_button')]", "Advanced Options Button");
+    this.advancedSearchWindow = new Label("//div[@id='market_advancedsearch_dialog']", "Advanced Search Window");
+    this.searchButton = new Button("//div[contains(@onclick, 'submit')]", "Search Button");
 
-    this.gameSelect = new Button("//div[@id='market_advancedsearch_appselect']", "Game Select")
-    this.gameOption = (gameName) => new Label(`//div[contains(@class, 'popup_menu')]//span[contains(., '${gameName}')]`, "Game Option")
-    this.heroSelect = new Dropdown("//select[contains(@name, 'Hero')]", "Hero Select")
-    this.rarityCheckbox = (rarityType) => new Label(`//label[contains(@for, 'Rarity_${rarityType}')]`, "Rarity Checkbox")
+    this.gameSelect = new Button("//div[@id='market_advancedsearch_appselect']", "Game Select");
+    this.gameOption = (gameName) => new Label(`//div[contains(@class, 'popup_menu')]//span[contains(., '${gameName}')]`, "Game Option");
+    this.heroSelect = new Dropdown("//select[contains(@name, 'Hero')]", "Hero Select");
+    this.rarityCheckbox = (rarityType) => new Label(`//label[contains(@for, 'Rarity_${rarityType}')]`, "Rarity Checkbox");
 
-    this.searchResultsTable = new Label("//div[@id='searchResultsRows']", "Search Results Table")
-    this.firstSearchItem = new Label("//a[@id='resultlink_0']", "First Search Item")
-    this.searchTags = new Label(`//div[contains(@class, 'search_results_header')]//div`, "Search Tags")
-    this.sortPriceButton = new Button("//div[@data-sorttype='price']", "Sort Price Button")
+    this.searchResultsTable = new Label("//div[@id='searchResultsRows']", "Search Results Table");
+    this.firstSearchItem = new Label("//a[@id='resultlink_0']", "First Search Item");
+    this.searchTags = new Label("//div[contains(@class, 'search_results_header')]//div", "Search Tags");
+    this.sortPriceButton = new Button("//div[@data-sorttype='price']", "Sort Price Button");
   }
 
   async showAdvancedOptions() {
-    this.advancedOptionsButton.click()
+    await this.advancedOptionsButton.click();
   }
 
-  async isAdvancedOptionsWindowOpened() {
-    return await this.advancedSearchWindow.state().waitForDisplayed()
+  isAdvancedOptionsWindowOpened() {
+    return this.advancedSearchWindow.state().waitForDisplayed();
   }
 
   async selectGameForSearch(gameName) {
-    await this.gameSelect.click()
-    await this.gameOption(gameName).moveTo()
-    await this.gameOption(gameName).click()
+    await this.gameSelect.click();
+    await this.gameOption(gameName).moveTo();
+    await this.gameOption(gameName).click();
   }
 
   async selectHeroForSearch(heroName) {
-    await this.heroSelect.selectOptionByText(heroName)
+    await this.heroSelect.selectOptionByText(heroName);
   }
 
   async selectRarityForSearch(rarityType) {
-    await this.rarityCheckbox(rarityType).click()
+    await this.rarityCheckbox(rarityType).click();
   }
 
   async clickSearchButton() {
-    await this.searchButton.click()
+    await this.searchButton.click();
   }
 
-  async isSearchResultsTableDisplayed() {
-    return await this.searchResultsTable.state().isDisplayed()
+  isSearchResultsTableDisplayed() {
+    return this.searchResultsTable.state().isDisplayed();
   }
 
   async areSearchTagsCorrect(expectedTags) {
-    const tagElementsArray = await this.searchTags.findAll(Label, "//a", "searchTag")
-    const searchTagArray = []
+    const tagElementsArray = await this.searchTags.findAll(Label, "//a", "searchTag");
+    const searchTagArray = [];
 
-    for await (let tagElement of tagElementsArray) {
-      const searchTagText = await tagElement.getText()
-      searchTagArray.push(searchTagText)
+    for (let tagElement of tagElementsArray) {
+      const searchTagText = await tagElement.getText();
+      searchTagArray.push(searchTagText);
     }
 
-    for (const tag of expectedTags) {
+    for (let tag of expectedTags) {
       if (!searchTagArray.includes(tag)) {
-        return false
+        return false;
       }
     }
 
-    return true
+    return true;
   }
 
   async clickOnFirstItem() {
-    await this.firstSearchItem.click()
+    await this.firstSearchItem.click();
   }
 
   async sortItems() {
-    await this.sortPriceButton.click()
+    await this.sortPriceButton.quickClick();
   }
 
   async areItemsSorted(order = "ascending") {
-    const searchItemsArray = await this.searchResultsTable.findAll(Label, "//span[@class='normal_price']", "searchItem")
-    const searchItemsPrices = []
+    await Browser.waitForDelay(3000);
 
-    for await (let searchItem of searchItemsArray) {
-      const searchItemText = await searchItem.getText()
-      searchItemsPrices.push(parseFloat(searchItemText.replace("$", "")))
+    const searchItemsArray = await this.searchResultsTable.findAll(Label, "//span[@class='normal_price']", "searchItem");
+    const searchItemsPrices = [];
+
+    for (let searchItem of searchItemsArray) {
+      const searchItemText = await searchItem.getText();
+      searchItemsPrices.push(parseFloat(searchItemText.replace("$", "")));
     }
 
     if (order === "ascending") {
-      return searchItemsPrices.every((price, index) => index === 0 || searchItemsPrices[index - 1] <= price)
+      return searchItemsPrices.every((price, index) => index === 0 || searchItemsPrices[index - 1] <= price);
     } else if (order === "descending") {
-      return searchItemsPrices.every((price, index) => index === 0 || searchItemsPrices[index - 1] >= price)
+      return searchItemsPrices.every((price, index) => index === 0 || searchItemsPrices[index - 1] >= price);
     }
   }
 }
 
-export default new CommunityMarketPage()
+export default new CommunityMarketPage();
